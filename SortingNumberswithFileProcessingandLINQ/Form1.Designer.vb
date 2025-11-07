a923185d627c7a07c6570d390c6e0982700621f2
7Louieee <hed-jldalapo@smu.edu.ph>
2025-11-07 19:19:22 +0800

    added clear button

--- SortingNumberswithFileProcessingandLINQ/Form1.Designer.vb
+++ SortingNumberswithFileProcessingandLINQ/Form1.Designer.vb
@@ -22,65 +22,76 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.btnSave = New System.Windows.Forms.Button()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.btnAddNumber = New System.Windows.Forms.Button()
         Me.btnSort = New System.Windows.Forms.Button()
-        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.lstSortedNumbers = New System.Windows.Forms.ListBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'txtInput
         '
-        Me.TextBox1.Location = New System.Drawing.Point(28, 53)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(254, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.txtInput.Location = New System.Drawing.Point(28, 53)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(254, 20)
+        Me.txtInput.TabIndex = 0
         '
-        'btnSave
+        'btnAddNumber
         '
-        Me.btnSave.Location = New System.Drawing.Point(28, 79)
-        Me.btnSave.Name = "btnSave"
-        Me.btnSave.Size = New System.Drawing.Size(64, 30)
-        Me.btnSave.TabIndex = 1
-        Me.btnSave.Text = "Save"
-        Me.btnSave.UseVisualStyleBackColor = True
+        Me.btnAddNumber.Location = New System.Drawing.Point(28, 79)
+        Me.btnAddNumber.Name = "btnAddNumber"
+        Me.btnAddNumber.Size = New System.Drawing.Size(64, 30)
+        Me.btnAddNumber.TabIndex = 1
+        Me.btnAddNumber.Text = "Write"
+        Me.btnAddNumber.UseVisualStyleBackColor = True
         '
         'btnSort
         '
-        Me.btnSort.Location = New System.Drawing.Point(288, 331)
+        Me.btnSort.Location = New System.Drawing.Point(28, 318)
         Me.btnSort.Name = "btnSort"
         Me.btnSort.Size = New System.Drawing.Size(64, 30)
         Me.btnSort.TabIndex = 2
-        Me.btnSort.Text = "sort"
+        Me.btnSort.Text = "Sort"
         Me.btnSort.UseVisualStyleBackColor = True
         '
-        'lstOutput
+        'lstSortedNumbers
         '
-        Me.lstOutput.FormattingEnabled = True
-        Me.lstOutput.Location = New System.Drawing.Point(28, 188)
-        Me.lstOutput.Name = "lstOutput"
-        Me.lstOutput.Size = New System.Drawing.Size(254, 173)
-        Me.lstOutput.TabIndex = 3
+        Me.lstSortedNumbers.FormattingEnabled = True
+        Me.lstSortedNumbers.Location = New System.Drawing.Point(28, 139)
+        Me.lstSortedNumbers.Name = "lstSortedNumbers"
+        Me.lstSortedNumbers.Size = New System.Drawing.Size(171, 173)
+        Me.lstSortedNumbers.TabIndex = 3
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(28, 34)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(81, 13)
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
         Me.Label1.TabIndex = 4
-        Me.Label1.Text = "Input a number:"
+        Me.Label1.Text = "Input Numbers:"
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(135, 318)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(64, 30)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(387, 385)
+        Me.ClientSize = New System.Drawing.Size(336, 385)
+        Me.Controls.Add(Me.btnClear)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.lstSortedNumbers)
         Me.Controls.Add(Me.btnSort)
-        Me.Controls.Add(Me.btnSave)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnAddNumber)
+        Me.Controls.Add(Me.txtInput)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -88,9 +99,10 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents btnSave As Button
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents btnAddNumber As Button
     Friend WithEvents btnSort As Button
-    Friend WithEvents lstOutput As ListBox
+    Friend WithEvents lstSortedNumbers As ListBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents btnClear As Button
 End Class
--- SortingNumberswithFileProcessingandLINQ/Form1.vb
+++ SortingNumberswithFileProcessingandLINQ/Form1.vb
@@ -1,3 +1,67 @@
 ﻿Imports System.IO
 Imports System.Linq
 
+Public Class Form1
+
+
+    Dim filePath As String = "numbers.txt"
+    Private Sub btnAddNumber_Click(sender As Object, e As EventArgs) Handles btnAddNumber.Click
+        Try
+
+            Dim userInput As String = txtInput.Text.Trim()
+
+            Dim number As Integer
+            If Integer.TryParse(userInput, number) Then
+
+
+                Using writer As New StreamWriter(filePath, True)
+                    writer.WriteLine(number)
+                End Using
+
+                MessageBox.Show("Number added successfully.", "Input Sucess.", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)
+                txtInput.Clear()
+            Else
+                MessageBox.Show("Please enter a valid number.")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error while adding number: " & ex.Message, "Error Has Occured!", MessageBoxButtons.OKCancel, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+        Try
+
+            Dim numbers As List(Of Integer) = New List(Of Integer)()
+
+            If File.Exists(filePath) Then
+                Using reader As New StreamReader(filePath)
+                    While Not reader.EndOfStream
+                        Dim line As String = reader.ReadLine()
+                        If Integer.TryParse(line, Nothing) Then
+                            numbers.Add(Integer.Parse(line))
+                        End If
+                    End While
+                End Using
+
+                Dim sortedNumbers = numbers.OrderBy(Function(n) n).ToList()
+
+                lstSortedNumbers.Items.Clear()
+                For Each number In sortedNumbers
+                    lstSortedNumbers.Items.Add(number)
+                Next
+
+                MessageBox.Show("Numbers sorted and displayed.")
+            Else
+                MessageBox.Show("No numbers found in the file.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error while reading or sorting numbers: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+    End Sub
+End Class
